30afe41e42a8f53a1e3091335f114d867af51237
dis-zephyro <zephyro@yandex.ru>
2015-11-26 04:25:51 +0300

    v1

---
+++ form-handler.php
@@ -0,0 +1,34 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $form = $_POST['form'];
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+	
+    require 'class.phpmailer.php';
+    $thm = 'Вам отправлено сообщение с сайта factorymebel - ' . $form;
+    $msg = "<strong>Форма:</strong> $form <br/>";
+    if(isset($name)){$msg .= "<strong>Имя:</strong> $name <br/>";}
+    if(isset($phone)){$msg .= "<strong>Телефон:</strong> $phone <br/>";}
+    if(isset($email)){$msg .= "<strong>Email:</strong> $email <br/>";}
+    if(isset($message)){$msg .= "<strong>Вопрос:</strong> $message <br/>";}
+
+
+    $mail = new PHPMailer();
+    $mail->From = 'rosich'; // от кого
+    $mail->FromName = 'modal-service'; // от кого
+    $mail->AddAddress('zephyro@yandex.ru', 'factorymebel'); // кому - адрес, Имя
+
+    $mail->IsHTML(true); // выставляем формат письма HTML
+    $mail->Subject = $thm; // тема письма
+    $mail->Body = $msg;
+
+    if (!$mail->Send()) die('Mailer Error: ' . $mail->ErrorInfo);
+
+    header("Location: " . $_SERVER["PHP_SELF"]);
+    exit;
+}
+?>
--- js/main.js
+++ js/main.js
@@ -6,3 +6,69 @@ $(".btn-modal").fancybox({
         closeBtn : '<a title="Close" class="btn_close" href="javascript:;"></a>'
     }
 });
+
+
+// Map
+
+ymaps.ready(init);
+
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [48.7162,44.5065],
+        zoom: 16,
+        controls: ['smallMapDefaultSet']
+    });
+
+    myPlacemark = new ymaps.Placemark([48.7162,44.5062], {
+        hintContent: ''
+    }, {
+        iconLayout: 'default#image',
+        iconImageHref: 'img/placemark.png',
+        iconImageSize: [47, 69],
+        iconImageOffset: [-23, -70]
+    });
+
+    myMap.behaviors.disable('scrollZoom');
+    myMap.behaviors.disable('multiTouch');
+    myMap.geoObjects.add(myPlacemark);
+}
+
+
+// ----- Маска ----------
+jQuery(function($){
+    $("input[name='phone']").mask("+7(999) 999-9999");
+});
+
+
+$(document).ready(function() {
+
+    $('.btn-submit').click(function() {
+
+        $('body').find('form:not(this)').children('div').removeClass('red'); //удаление всех сообщение об ошибке(валидатора)
+        var answer = checkForm($(this).closest('form').get(0)); //ответ от валидатора
+        if(answer != false)
+        {
+            var $form = $(this).closest('form'),
+                name    =     $('input[name="name"]', $form).val(),
+                type    =     $('input[name="type"]', $form).val(),
+                phone   =     $('input[name="phone"]', $form).val(),
+                email   =     $('input[name="email"]', $form).val(),
+                form    =     $('input[name="form"]', $form).val(),
+                message =     $('textarea[name="message"]', $form).val();
+            console.log(name, phone, email, form, type, message);
+            $.ajax({
+                type: "POST",
+                url: "form-handler.php",
+                data: {name: name, phone: phone, email:email, form:form, type:type, message:message}
+            }).done(function(msg) {
+                $('form').find('input[type=text], textarea').val('');
+                console.log('удачно');
+                document.location.href = "../done.html";
+            });
+        }
+    });
+
+});
